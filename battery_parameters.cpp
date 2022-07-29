f9dd288a141246d0585f43e68c122381b5583330
arpit091429 <arpitkhandelwal24@gmail.com>
2022-07-29 19:28:23 +0530

    Update battery_parameters.cpp

--- battery_parameters.cpp
+++ battery_parameters.cpp
@@ -1,74 +1,45 @@
-#include<iostream>
-#include<algorithm>
-#include"battery_parameters.h"
+#include "battery_parameters.h"
+#include <string>
 
-
+#define TOTAL_COUNT 50
 
 std::vector<int> BMSParameters::generateRandomPrameters(int minVal, int maxVal)
 {
-	static std::vector<int> generatedParameters;
-	generatedParameters.push_back(rand() % (maxVal - minVal + 1) + minVal);
-    return generatedParameters;        
+    static std::vector<int> generatedParameters;
+    generatedParameters.push_back(rand() % (maxVal - minVal + 1) + minVal);
+    return generatedParameters;
 }
 
 std::vector<int> BMSParameters::getBatteryTempValuesInRange(int minTempVal, int maxTempVal)
 {
-    return generateRandomPrameters(minTempVal, maxTempVal);         
+    return generateRandomPrameters(minTempVal, maxTempVal);
 }
 
 std::vector<int> BMSParameters::getBatterySOCValuesInRange(int minSOCVal, int maxSOCVal)
 {
-    return generateRandomPrameters(minSOCVal, maxSOCVal);         
+    return generateRandomPrameters(minSOCVal, maxSOCVal);
 }
 
-void BMSParameters::sendTemperatureValuesToConsole(int minTempVal, int maxTempVal)
+template <typename T>
+void BMSParameters::sendParamValuesToConsole(T min, T max, std::string parameterName)
 {
-    int numberOfValues = 50;
-    printf("The values for parameter: Temperature");
+
+    std::cout << "The values for parameter:";
+    std::cout << parameterName;
     std::cout << "------------------------------------------------------\n";
-    for (int paramValue = 0; paramValue < numberOfValues; ++paramValue)
+    for (int paramValue = 0; paramValue < TOTAL_COUNT; ++paramValue)
     {
-        send_array_temp[paramValue] = getBatteryTempValuesInRange(minTempVal, maxTempVal)[paramValue];
-        if (paramValue < numberOfValues - 1)
-            std::cout << send_array_temp[paramValue] << ",";
+        send_array_param[paramValue] = getBatterySOCValuesInRange(min, max)[paramValue];
+        if (paramValue < TOTAL_COUNT - 1)
+            std::cout << send_array_param[paramValue] << ",";
         else
-            std::cout << send_array_temp[paramValue];
+            std::cout << send_array_param[paramValue];
     }
     std::cout << "\n";
 }
 
-void BMSParameters::sendSOCValuesToConsole(int minSOCVal, int maxSOCVal)
+bool BMSParameters::sendParameterValuesToConsole(std::string parameterName, int minVal, int maxVal)
 {
-    int numberOfValues = 50;
-
-    printf("The values for parameter: SOC");
-    std::cout << "------------------------------------------------------\n";
-    for (int paramValue = 0; paramValue < numberOfValues; ++paramValue)
-    {
-        send_array_soc[paramValue] = getBatterySOCValuesInRange(minSOCVal, maxSOCVal)[paramValue];
-        if (paramValue < numberOfValues - 1)
-            std::cout << send_array_soc[paramValue] << ",";
-        else
-            std::cout << send_array_soc[paramValue];
-    }
-    std::cout << "\n";
-}
-
-bool BMSParameters::sendParameterValuesToConsole( std::string parameterName, int minVal, int maxVal)
-{
-	if(parameterName == "Temperature")
-	{
-		sendTemperatureValuesToConsole(minVal,maxVal );
-		return true;
-	}
-	if(parameterName == "SOC")
-	{
-		sendSOCValuesToConsole(minVal,maxVal );
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-
+    sendParamValuesToConsole(minVal, maxVal, parameterName);
+    return true;
 }
